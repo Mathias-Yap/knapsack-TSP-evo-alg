82f046fa81ce3e542e8f7f052461598f551a4583
MathiasYap <yapmathias@gmail.com>
2024-03-17 20:08:10 +0100

    more import fixes

--- src/problems/TSP.py
+++ src/problems/TSP.py
@@ -1,4 +1,7 @@
 from abstract_problem import problem
 class TSP(problem):
-     def __init__(self):
-         
+     def __init__(self,n_cities: int):
+         """TSP problem with 0,0 at the center point. Cities lie on the unit circle that surrounds this center point. Cities are represented by their angle from this center point"""
+     
+     def make_cities(n_cities):
+          
